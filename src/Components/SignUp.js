5dfe1fc1b329a89804773dfeded94082210b65dd
Machoka Joash Omao <aaronjoash44@gmail.com>
2023-01-10 14:53:19 +0300

    implemented sign up journey

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,47 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
- function SignUp() {
-  return (
-    <div className='signup-form-box'>
-    <h1 className='signup-title'>New Users Sign Up Here!</h1>
-    <form >
-    
-      {/* <label htmlFor="username">Username:</label> */}
-      <input
-      placeholder='Username'
-        type="text"
-        id="username"
-      
-      />
-      <input
-      placeholder='Email'
-        type="email"
-        id="email"
-      
-      />
-       <input
-      placeholder='Phone Number'
-        type="number"
-        id="phone_number"
-      
-      />
-      <input
-      placeholder='Password'
-        type="password"
-        id="password"
-       
-      />
-      <input
-      placeholder='Confirm Password'
-        type="password"
-        id="password_confirmation"
+import React, { useState } from "react";
+import Stepper from "./Stepper";
+import StepperControl from "./StepperControl";
+import { UseContextProvider } from "../contexts/StepperContext";
 
-      />
-      <button className="signup-button" type="submit">Sign Up</button>
-    </form>
-    <h3>Aleady have an account?
-                <Link to="/login" className="login-button">Log In</Link>
-            </h3>
+import Account from "./steps/Account";
+import Details from "./steps/Details";
+import Payment from "./steps/Payment";
+import Final from "./steps/Final";
+import './SignUp.css'
+
+function SignUp() {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const steps = [
+    "Account Information",
+    "Personal Details",
+    "Payment",
+    "Complete",
+  ];
+
+  const displayStep = (step) => {
+    switch (step) {
+      case 1:
+        return <Account />;
+      case 2:
+        return <Details />;
+      case 3:
+        return <Payment />;
+      case 4:
+        return <Final />;
+      default:
+    }
+  };
+
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+
+    direction === "next" ? newStep++ : newStep--;
+    // check if steps are within bounds
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  };
+
+  return (
+    <div className="mx-auto rounded-2xl bg-white pb-2 shadow-xl md:w-1/2">
+      {/* Stepper */}
+      <div className="horizontal container mt-5 ">
+        <Stepper steps={steps} currentStep={currentStep} />
+
+        <div className="my-10 p-10 ">
+          <UseContextProvider>{displayStep(currentStep)}</UseContextProvider>
+        </div>
+      </div>
+
+      {/* navigation button */}
+      {currentStep !== steps.length && (
+        <StepperControl
+          handleClick={handleClick}
+          currentStep={currentStep}
+          steps={steps}
+        />
+      )}
     </div>
   );
 }
