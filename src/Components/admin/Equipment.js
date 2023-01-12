50c31425472fe65f83c51983e4fd468cba880156
Masai11 <roy.kiplagat@student.moringaschool.com>
2023-01-12 23:37:39 +0300

    add admin dashboard

--- src/Components/admin/Equipment.js
+++ src/Components/admin/Equipment.js
@@ -2,6 +2,63 @@ import React from 'react'
 
 export default function Equipment() {
   return (
-    <div id="equipment">Equipment</div>
+    <div className="table">
+      <h1>Equipment</h1>
+      <table>
+        <thead className="head">
+          <tr>
+            <th>Avatar</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Condition</th>
+            <th>Number</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <td>
+              <img
+                className="avatar"
+                src="https://images.pexels.com/photos/14007218/pexels-photo-14007218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </td>
+            <td>1</td>
+            <td>Mark</td>
+            <td>0728300345</td>
+            
+            <td>complete</td>
+          </tr>
+          <tr>
+          <td>
+              <img
+                className="avatar"
+                src="https://images.pexels.com/photos/14007218/pexels-photo-14007218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </td>
+            <td>2</td>
+
+            <td>Mark</td>
+            <td>0728300345</td>
+            
+            <td>complete</td>
+          </tr>
+          <tr>
+          <td>
+              <img
+                className="avatar"
+                src="https://images.pexels.com/photos/14007218/pexels-photo-14007218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </td>
+            <td>3</td>
+
+            <td>Mark</td>
+            <td>0728300345</td>
+            
+            <td>complete</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
--- src/Components/admin/NavBaradmin.js
+++ src/Components/admin/NavBaradmin.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Admin.css";
+import { Link } from "react-router-dom";
+
 
 export default function NavBaradmin() {
-    return (
-        <div className='nav-admin'>
-       
-            
-                <li className='dash-links'><a href='#clients'>Clients</a></li>
-                <li className='dash-links'><a href='#equipment'>Equipment</a></li>
-                <li className='dash-links'><a href='#trainers'>Trainers</a></li>
-            
-        </div>
-    )
+  return (
+    <div className="nav-admin">
+      <div className="cli">
+        <NavLink className="adm-links" style={{ textDecoration: "none" }} to="/client">
+          <a >Clients</a>
+        </NavLink>
+        <NavLink className="adm-links" style={{ textDecoration: "none" }} to="/trainers">
+          <a >Trainers</a>
+        </NavLink>
+        <NavLink className="adm-links"style={{ textDecoration: "none" }} to="/equipment">
+          <a>Equipment</a>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
