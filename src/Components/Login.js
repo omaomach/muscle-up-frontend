c59df60e2503d6dbef34bcf6a2424f645bb686ea
gracekyalo <gracendanu27@gmail.com>
2023-01-16 12:08:02 +0300

    Updated Login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,29 +3,37 @@ import { Link } from 'react-router-dom';
 function Login() {
   return (
     <div className='login-form-box'>
-      <h1 className='login-title'> Log In Here!</h1>
+      <div className='title'>
+        <h1 className='login-title'> Log In Here!</h1>
+      </div>
+      <div className='abt-form'>
+        <form className='form'>
 
-      <form className='form'>
+          <input
+            placeholder='Username'
+            type="text"
 
-        <input
-        placeholder='Username'
-          type="text"
+          />
 
-        />
+          <input
+            placeholder='Password'
+            type="password"
+          />
+          <button className='login-btn' type="submit">Login</button>
 
-        <input
-        placeholder='Password'
-          type="password"
-        />
-        <button className='login-button' type="submit">Login</button>
-  
-      </form>
-      <h3>Don't have an account?
-                    <Link to="/signup" className="link">Sign Up</Link>
-                </h3>
-                </div>
-   
-   
+        </form>
+      </div>
+      <div>
+        <h3 className='abt-header'>Don't have an account?
+        </h3>
+      </div>
+      <div>
+      <Link to="/signup" className="signup-btn">Sign Up</Link>
+
+      </div>
+    </div>
+
+
   );
 }
 
