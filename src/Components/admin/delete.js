51c125953ab95bc9f72bfa073a8fcc16a6407876
Muthiira <calebmuchiri48@gmail.com>
2023-01-18 00:34:21 +0300

    equipmentdelete

--- src/Components/admin/delete.js
+++ src/Components/admin/delete.js
@@ -3,3 +3,16 @@ export function Delete(id){
 		method : "DELETE",
 	}).then((resp) => resp.json()) 
 }
+
+export function tDelete(id){
+	return fetch(`http://127.0.0.1:3000/trainers/${id}` ,{
+		method : "DELETE",
+	}).then((resp) => resp.json()) 
+}
+
+
+export function eDelete(id){
+	return fetch(`http://127.0.0.1:3000/equipment/${id}` ,{
+		method : "DELETE",
+	}).then((resp) => resp.json()) 
+}
